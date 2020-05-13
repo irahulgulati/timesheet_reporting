3cad8e6dd30fa063048a086080250d920cb507d5
Rahul <gulati.rg65@gmail.com>
2020-05-13 01:18:12 -0300

    Working version v1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Time Reporting script
+## Created by irahulgulati
+
+# Setup
+1. Python 3.7.2
+2. RUN "pip install -r requirements.txt"
+3. Place Excel sheet or use the template one already provided inside timesheets folder
+4. Keep on updating the same tab inside excel file everyday
+5. Use Below command to run it.
+  - python automate_reporting.py [filename] [Your Name]
+  - Should run above command inside the same directory as automate_reporting.py file is present.
---
+++ automate_reporting.py
@@ -0,0 +1,134 @@
+import xlrd as excel
+import os
+from datetime import date
+import time
+import sys
+import requests
+import json
+import argparse
+
+
+class timesheet:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            description='**Automated Timesheet Reporting**', usage="automate_reporting.py [-h] ||[filename.xlsx] [your name]")
+        self.parser.add_argument('file', type=str,
+                                 help='file name with .xlsx extension')
+        self.parser.add_argument('name', type=str,
+                                 help='Your Name')
+
+        self.args = self.parser.parse_args()
+        self._cachedTime = 0
+        # Setting the base directory and file paths
+        self.basedir = os.path.abspath(os.path.dirname(__file__))
+        self.timesheetDir = self.basedir + '\\timesheets'
+
+    # main logic
+
+    def fileCheck(self):
+        self.sheetlocation = (os.path.join(
+            self.timesheetDir, self.args.file))
+
+        # initiating the excel sheet
+        self.wb = excel.open_workbook(self.sheetlocation)
+
+        # setting up the start poing with cell index
+        self.sheet = self.wb.sheet_by_index(0)
+        self.sheet.cell_value(0, 0)
+        self.numberofRows = self.sheet.nrows
+        self.records = []
+        for entry in range(1, self.numberofRows):
+            self.records.append(self.sheet.row_values(entry))
+        data = {
+            "blocks": [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Name\n\n"+self.args.name
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Date: \n" + str(date.today()),
+                    }
+                },
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": str(self.records[0])
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": str(self.records[1])
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": str(self.records[2])
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": str(self.records[3])
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": str(self.records[4])
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": str(self.records[5])
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": str(self.records[6])
+                    }
+                },
+            ]
+        }
+        if os.stat(self.sheetlocation).st_mtime != self._cachedTime:
+            requests.post(
+                'https://hooks.slack.com/services/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', data=json.dumps(data))
+        else:
+            # print(self._cachedTime)
+            print("False")
+
+
+if __name__ == "__main__":
+    try:
+        time.sleep(1)
+        # instantiating the class object
+        run = timesheet()
+        run.fileCheck()
+    except Exception as err:
+        print(err)
+        sys.exit(0)
